0322c2d1745bd8e3d55124d1268ef6de98832f4c
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-04 20:30:16 -0300

    Css from form

--- 10_MINIBLOG/miniblog/src/pages/Register/Register.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.jsx
@@ -1,9 +1,31 @@
 import styles from './Register.module.css'
 
+import { useState, useEffect } from 'react'
+
 const Register = () => {
   return (
     <div>
-        <h2>Register</h2>
+        <h1>Cadastra-se para postar</h1>
+        <p>Crie seu usuario e compartilhe suas historias</p>
+        <form>
+            <label>
+                <span>Nome:</span>
+                <input type="text" name='displayName' required placeholder='Nome do usuario' />
+            </label>
+            <label>
+                <span>E-mail:</span>
+                <input type="email" name='email' required placeholder='E-mail do usuario' />
+            </label>
+            <label>
+                <span>Senha:</span>
+                <input type="password" name='password' required placeholder='Insira sua senha' />
+            </label>
+            <label>
+                <span>Confirmacao de senha:</span>
+                <input type="password" name='confirmPassword' required placeholder='Confirme a sua senha' />
+            </label>
+            <button className='btn'>Cadastrar</button>
+        </form>
     </div>
   )
 }
