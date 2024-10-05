ae0c785441600256154062114f9fb5515aa2f495
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-05 14:29:58 -0300

    Passing user to context

--- 10_MINIBLOG/miniblog/src/context/AuthContext.jsx
+++ 10_MINIBLOG/miniblog/src/context/AuthContext.jsx
@@ -6,6 +6,8 @@ export function AuthProvider({children, value}){
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
 
+
+
 export function useAuthValue(){
     return useContext(AuthContext)
 }
