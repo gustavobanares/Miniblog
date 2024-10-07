d842ab9ad2c69f97a1732df5cf855877256e9deb
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-07 19:11:51 -0300

    Login of user

--- 10_MINIBLOG/miniblog/src/hooks/useAuthentication.jsx
+++ 10_MINIBLOG/miniblog/src/hooks/useAuthentication.jsx
@@ -66,8 +66,8 @@ export const useAuthentication = () =>{
                 systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
             }
             
-            setLoading(false)
             setError(systemErrorMessage)
+            setLoading(false)
 
         } 
         
@@ -83,6 +83,37 @@ export const useAuthentication = () =>{
 
     }
 
+    // login - sign in
+    const login = async(data) =>{
+
+        checkIfIsCancelled()
+
+        setLoading(true)
+        setError(false)
+
+        try {
+            
+            await signInWithEmailAndPassword(auth, data.email, data.password)
+            setLoading(false)
+
+        } catch (error) {
+            
+            let systemErrorMessage
+
+            if(error.message.includes('user-not-found')){
+                systemErrorMessage = 'Usuario nao encontrado.'
+            } else if(error.message.includes('wrong-password')){
+                systemErrorMessage = 'Senha incorreta.'
+            } else{
+                systemErrorMessage = 'Ocorreu um erro, por favor tente mais tarde.'
+            }
+
+            setError(systemErrorMessage)
+            setLoading(false)   
+        }
+
+    }
+
     useEffect(() =>{
         return() => setCancelled(true)
     }, [])
@@ -93,5 +124,6 @@ export const useAuthentication = () =>{
         error,
         loading,
         logout,
+        login,
     }
 }
--- 10_MINIBLOG/miniblog/src/pages/Login/Login.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Login/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
 
-  const {createUser, error: authError, loading} = useAuthentication()
+  const {login, error: authError, loading} = useAuthentication()
 
   const handleSubmit = async (e) =>{
       e.preventDefault()
@@ -21,8 +21,7 @@ const Login = () => {
           email,
           password,
       }
-
-      const res = await createUser(user)
+      const res = await login(user)
 
       console.log(res)
   }
