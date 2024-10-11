806856ac6ac18caedf4cb6b834fad1bf5f9ea9a1
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-11 18:25:36 -0300

    CSS from dashboard

--- 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
+++ 10_MINIBLOG/miniblog/src/pages/Dashboard/Dashboard.jsx
@@ -20,7 +20,7 @@ if(loading) {
 }
 
   return (
-    <div>
+    <div className={styles.dashboard}>
         <h2>Dashboard</h2>
         <p>Gerencie o seus posts</p>
         {posts && posts.length === 0 ? (
@@ -30,12 +30,12 @@ if(loading) {
           </div>
         ) : (
          <>
-          <div>
-            <span>Titulo</span>
-            <span>Acoes</span>
+          <div className={styles.post_header}>
+            <span>Título</span>
+            <span>Ações</span>
           </div>
           
-          { posts && posts.map((post) => <div key={post.id}>
+          { posts && posts.map((post) => <div key={post.id} className={styles.post_row}>
             <p>{post.title}</p>
             <div>
               <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ver</Link>
