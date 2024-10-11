240284da3e0b5544c1e8ea14702a5a276e43d566
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-10-10 22:30:18 -0300

    Update hook useFetchDocuments

--- 10_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
+++ 10_MINIBLOG/miniblog/src/hooks/useFetchDocuments.jsx
@@ -1,53 +1,56 @@
-import {useState, useEffect} from 'react'
-import {db} from '../firebase/config'
-import { collection, query, orderBy, onSnapshot, where, QuerySnapshot } from 'firebase/firestore'
+import { useState, useEffect } from 'react';
+import { db } from '../firebase/config';
+import { collection, query, orderBy, onSnapshot, where } from 'firebase/firestore';
 
 export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+  const [documents, setDocuments] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-    const [documents, setDocuments] = useState(null)
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(null)
+  const [cancelled, setCancelled] = useState(false);
 
-    // deal with memory leak
-    const [cancelled, setCancelled] = useState(false);
+  useEffect(() => {
+    const loadData = async () => {
+      if (cancelled) return;
 
-    useEffect(() =>{
+      setLoading(true);
+      const collectionRef = collection(db, docCollection);
 
-        const loadData = async () => {
-            if (cancelled) return;
-       
-            setLoading(true);
-       
-            const collectionRef = await collection(db, docCollection);
-       
-            try {
-              let q = await query(
-                collectionRef,
-                orderBy("createdAt", "desc")
-              );
-       
-              await onSnapshot(q, (querySnapshot) => {
-                setDocuments(
-                  querySnapshot.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                  }))
-                );
-              });
-       
-              setLoading(false);
-            } catch (error) {
-              setError(error.message);
-              setLoading(false);
-            }
-          };
-          loadData();
+      try {
+        let q;
+        // Condicional para buscar por tags se a query for passada
+        if (search) {
+          q = query(
+            collectionRef,
+            where('tags', 'array-contains', search),
+            orderBy('createdAt', 'desc')
+          );
+        } else {
+          q = query(collectionRef, orderBy('createdAt', 'desc'));
+        }
 
-    }, [docCollection, search, uid, cancelled])
+        // Realiza a consulta e escuta as atualizações
+        onSnapshot(q, (querySnapshot) => {
+          setDocuments(
+            querySnapshot.docs.map((doc) => ({
+              id: doc.id,
+              ...doc.data(),
+            }))
+          );
+          setLoading(false);
+        });
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+    
+    loadData();
+  }, [docCollection, search, uid, cancelled]);
 
-    useEffect(() =>{
-        return () => setCancelled(true)
-    }, [])
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
 
-    return {documents, loading, error}
-}
+  return { documents, loading, error };
+};
